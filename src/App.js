d5dd601a87aaa3864b7348f63e3d61da4f00e6a9
haneen <haneenjaradeh1@gmail.com>
2021-09-26 15:54:14 +0300

    lab08

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ class App extends React.Component {
   };
 
   getWeather = async (event) => {
-    let reqWeatherUrl = `${process.env.REACT_APP_SERVER_LINK}/getWeather?city=${this.state.searchQuery}&`;
+    //${process.env.REACT_APP_SERVER_LINK}/getWeather?city=${this.state.searchQuery}&
+    let reqWeatherUrl = `https://lab03d33-lab08-backend.herokuapp.com/getWeather?city=${this.state.searchQuery}&days=4`;
     console.log(reqWeatherUrl);
     let weatherResult = await axios.get(reqWeatherUrl);
     console.log(weatherResult);
@@ -69,7 +70,8 @@ class App extends React.Component {
   };
 
   getMovie = async (event) => {
-    let reqMovieUrl = `${process.env.REACT_APP_SERVER_LINK}/getMovie?searchQuery=${this.state.searchQuery}`;
+    //`${process.env.REACT_APP_SERVER_LINK}/getMovie?searchQuery=${this.state.searchQuery}`;
+    let reqMovieUrl = `https://lab03d33-lab08-backend.herokuapp.com/getMovie?searchQuery=${this.state.searchQuery}`;
     console.log(reqMovieUrl);
     let movieResult = await axios.get(reqMovieUrl);
     console.log(movieResult);
@@ -215,15 +217,12 @@ class App extends React.Component {
             })}
 
             {/* 
-
             <p style={{ fontWeight: 'bold' }}>
               Description: {this.state.locationResult[0].description}
             </p>
-
             <p style={{ fontWeight: 'bold' }}>
               Description: {this.state.locationResult[1].description}{' '}
             </p>
-
             <p style={{ fontWeight: 'bold' }}>
               Description: {this.state.locationResult[2].description}{' '}
             </p> */}
